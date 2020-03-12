00309a15cb3e678fae8c917983a45c60bcc2b827
Sudakov <Sudakov.oleg123@yandex.ru>
2020-03-12 20:05:14 +0300

    Добавил класс матрицы реализованый на стондартных векторах TODO - по мере разработки при необходимости возможно нужно будт изменить функцию take

---
+++ matrix.h
@@ -0,0 +1,20 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <vector>
+#include <QSize>
+
+class Matrix
+{
+public:
+  Matrix(int _m, int _n);
+  int *take(int i, int j);
+  void resize(int _m, int _n);
+  QSize size();
+  Matrix &operator=(const Matrix&);
+private:
+  std::vector<std::vector<int>> matrix;
+  int m,n;
+};
+
+#endif // MATRIX_H
